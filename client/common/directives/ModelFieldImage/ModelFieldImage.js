1f7eae202b4a345f882ca24b93432859a8b3ffbd
John Meeker <john.meeker@science37.com>
2018-02-06 18:00:56 -0800

    Use ng-class to handle rendering of the image container

--- client/common/directives/ModelFieldImage/ModelFieldImage.js
+++ client/common/directives/ModelFieldImage/ModelFieldImage.js
@@ -25,7 +25,7 @@ angular.module('dashboard.directives.ModelFieldImage', [
   return {
     restrict: 'E',
     require: '^form',
-    template: '<div class="image-container" ng-class="{ \'display-only\' : displayOnly }" style="background: no-repeat center center url(\'{{ thumbnailUrl }}\'); background-size: contain;" ng-click="!displayOnly && imageClick()"></div> \
+    template: '<div ng-class="isDisplayOnly ? \'display-only\' : \'image-container\'" style="background: no-repeat center center url(\'{{ thumbnailUrl }}\'); background-size: contain;" ng-click="!isDisplayOnly && imageClick()"></div> \
       <div class="button-menu show-menu">\
       <button class="btn btn-default upload-button" ng-hide="disabled">{{ selectFileButtonText }}</button> \
       <button class="btn btn-default clear-button" ng-show="imageUrl && !disabled" ng-click="clear()">{{ clearButtonText }}</button> \
@@ -63,9 +63,8 @@ angular.module('dashboard.directives.ModelFieldImage', [
         scope.uploadStatus = "Upload File";
 
         // For question type image-display
-        if (scope.options.displayOnly) {
-          scope.displayOnly = true;
-          scope.imageUrl = scope.options.imageUrl;
+        if (scope.options.isDisplayOnly) {
+          scope.isDisplayOnly = true;
           scope.thumbnailUrl = scope.options.imageUrl;
         }
 
